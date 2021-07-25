33a168260f9a8958c1726f7e890adb179d2e8600
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-07-25 21:58:14 +0530

    Create output.tf
    Initial commit

---
+++ output.tf
@@ -0,0 +1,61 @@
+
+
+variable "region" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "sgp" {
+  type = list(any)
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "product" {
+  type = string
+}
+
+variable "tower" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "mail" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "place" {
+  type = string
+}
+
+variable "country" {
+  type = string
+}
+
+variable "division" {
+  type = string
+}
